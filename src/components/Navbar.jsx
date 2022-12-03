571e820106ac46da08dfc66b91e24a344d8df5cf
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-12-03 18:38:25 +0100

    donNavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,8 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
     >
       <span style={{background: dotColor}}
       className="absolute inline-flex
-      rounded-full h-2 w-2 right-2 top-2">
+      rounded-full h-2 w-2 right-2 top-2" />
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 );
