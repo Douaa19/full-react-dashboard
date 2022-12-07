03a68da250623f76a6c069df2f75f8bc9823159a
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-12-07 01:06:59 +0100

    RevenuUpdatesErrorNoChartsShowed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -4,24 +4,21 @@ import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-re
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
     <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName="x"
-      yName="y"
-      type={type}
-      tooltipSettings={{
+    id={id}
+    height={height}
+    width={width}
+    lineWidth={1}
+    valueType="Numeric"
+    fill={color}
+    border={{ color: currentColor, width: 2 }}
+    tooltipSettings={{
+      visible: true,
+      // eslint-disable-next-line no-template-curly-in-string
+      format: '${x} : data ${yval}',
+      trackLineSettings: {
         visible: true,
-        format: '${x} : data ${y}',
-        trackLineSettings: {
-          visible: true,
-        }
-      }}
+      },
+    }}
     >
       <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
