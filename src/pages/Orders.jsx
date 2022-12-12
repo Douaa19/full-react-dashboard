3ad91b0c3f34ed0a01f6aaba6b4864ffaa3ddc82
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-12-12 10:49:39 +0100

    displayOrdersData

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -20,7 +20,10 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-1à bg-white rounded-3xl'>
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
